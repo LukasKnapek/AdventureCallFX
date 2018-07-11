054ee770cbfd719a9ebb18095620572756d1758a
Lukas Knapek <lukas.knapek@kolabnow.com>
2018-07-11 18:12:46 +0200

    Add/Remove StoryPiece methods

--- src/org/mabufudyne/core/Adventure.java
+++ src/org/mabufudyne/core/Adventure.java
@@ -1,25 +1,49 @@
 package org.mabufudyne.core;
 
+import java.util.ArrayList;
+
 public class Adventure {
 
     private static String DEFAULT_NAME = "New Adventure";
+    private ArrayList<StoryPiece> storyPieces;
 
     private String name;
 
     public Adventure() {
-        this.name = DEFAULT_NAME;
+        this(DEFAULT_NAME);
     }
 
     public Adventure(String name) {
         this.name = name;
+        this.storyPieces = new ArrayList<>();
+
+        StoryPiece initialSP = new StoryPiece();
+        storyPieces.add(initialSP);
     }
 
     public String getName() {
         return this.name;
     }
 
+    public  ArrayList<StoryPiece> getStoryPieces() {
+        return this.storyPieces;
+    }
+
     public static String getDefaultName() {
         return DEFAULT_NAME;
     }
+
+    public void addStoryPiece(StoryPiece sp) {
+        if (!storyPieces.contains(sp)) {
+            this.storyPieces.add(sp);
+        }
+    }
+
+    public void removeStoryPiece(StoryPiece sp) {
+        if (this.storyPieces.size() > 1) {
+            this.storyPieces.remove(sp);
+        }
+    }
+
 
 }
--- test/org/mabufudyne/core/AdventureTest.java
+++ test/org/mabufudyne/core/AdventureTest.java
@@ -3,6 +3,8 @@ package org.mabufudyne.core;
 import static org.junit.Assert.*;
 import org.junit.Test;
 
+import java.util.Collections;
+
 public class AdventureTest {
 
     @Test
@@ -18,4 +20,105 @@ public class AdventureTest {
         assertEquals("The Adventure name should be the default name",
                 Adventure.getDefaultName(), testAdv.getName());
     }
+
+    @Test
+    public void Constructor_ShouldCreateAnAdventureWithOneStoryPiece() {
+        Adventure testAdv = new Adventure();
+        assertEquals("The Adventure has to contain at least one StoryPiece at all times," +
+                        " including immediately after creation",
+                1, testAdv.getStoryPieces().size());
+    }
+
+    @Test
+    public void AddStoryPiece_ShouldAddStoryPieceToTheAdventure_GivenAStoryPiece() {
+        Adventure testAdv = new Adventure();
+        StoryPiece sp = new StoryPiece();
+        testAdv.addStoryPiece(sp);
+        assertTrue("The Adventure does not contain the StoryPiece that was added to it",
+                testAdv.getStoryPieces().contains(sp));
+    }
+
+    @Test
+    public void AddStoryPiece_ShouldAddStoryPiecesToTheAdventure_GivenMultipleStoryPieces() {
+        Adventure testAdv = new Adventure();
+        StoryPiece[] storyPieces = new StoryPiece[5];
+        for (int i=0; i<5; i++) {
+            storyPieces[i] = new StoryPiece();
+            testAdv.addStoryPiece(storyPieces[i]);
+        }
+
+        for (StoryPiece sp : storyPieces) {
+            assertTrue("The Adventure does not contain one of the StoryPieces that were added to it",
+                    testAdv.getStoryPieces().contains(sp));
+        }
+    }
+
+    @Test
+    public void AddStoryPiece_ShouldNotAddStoryPieceToTheAdventure_GivenTheAdventureAlreadyContainsTheStoryPiece() {
+        Adventure testAdv = new Adventure();
+        StoryPiece sp = new StoryPiece();
+
+        testAdv.addStoryPiece(sp);
+        testAdv.addStoryPiece(sp);
+
+        assertTrue("The Adventure should not add the StoryPiece if it already exists inside the Adventure",
+                Collections.frequency(testAdv.getStoryPieces(), sp) == 1);
+    }
+
+    @Test
+    public void RemoveStoryPiece_ShouldRemoveStoryPiece_GivenTheStoryPieces() {
+        Adventure testAdv = new Adventure();
+        StoryPiece sp = new StoryPiece();
+
+        testAdv.addStoryPiece(sp);
+        testAdv.removeStoryPiece(sp);
+
+        assertFalse("The Adventure should not contain the StoryPiece that was removed from it",
+                testAdv.getStoryPieces().contains(sp));
+    }
+
+    @Test
+    public void RemoveStoryPiece_ShouldRemoveStoryPieces_GivenTheStoryPieces() {
+        Adventure testAdv = new Adventure();
+        StoryPiece[] storyPieces = new StoryPiece[5];
+        for (int i=0; i<5; i++) {
+            storyPieces[i] = new StoryPiece();
+            testAdv.addStoryPiece(storyPieces[i]);
+        }
+
+        for (StoryPiece sp : storyPieces) {
+            testAdv.removeStoryPiece(sp);
+            assertFalse("The Adventure should not contain one of the StoryPieces that were removed from it",
+                    testAdv.getStoryPieces().contains(sp));
+        }
+    }
+
+    @Test
+    public void RemoveStoryPiece_ShouldNotRemovyStoryPiece_GivenItIsTheLastOne() {
+        Adventure testAdv = new Adventure();
+        StoryPiece sp = testAdv.getStoryPieces().get(0);
+        testAdv.removeStoryPiece(sp);
+        assertTrue("The Adventure should not have removed the last StoryPiece it has",
+                testAdv.getStoryPieces().contains(sp));
+    }
+
+    @Test
+    public void RemoveStoryPiece_ShouldRemoveStoryPiecesExceptForTheLastOne_GivenTheStoryPieces() {
+        Adventure testAdv = new Adventure();
+        StoryPiece[] storyPieces = new StoryPiece[5];
+        storyPieces[0] = testAdv.getStoryPieces().get(0);
+
+        for (int i=1; i<5; i++) {
+            storyPieces[i] = new StoryPiece();
+            testAdv.addStoryPiece(storyPieces[i]);
+        }
+
+        for (StoryPiece sp : storyPieces) {
+            testAdv.removeStoryPiece(sp);
+            assertTrue("The Adventure should not have removed the last StoryPiece",
+                    testAdv.getStoryPieces().size() > 0);
+        }
+    }
+
+
 }
