e69f8481c7f1fc2c1baccb8fc3a8410ae179ebc7
Mabufudyne <lukas.knapek@kolabnow.com>
2018-12-25 15:27:03 +0100

    StoryPieceView UI FXML binding with controller

---
+++ src/main/java/org/mabufudyne/designer/gui/StoryPieceViewController.java
@@ -0,0 +1,10 @@
+package org.mabufudyne.designer.gui;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.*;
+
+public class StoryPieceViewController {
+    @FXML private Spinner<Integer> spinOrder;
+    @FXML private TextField textTitle;
+    @FXML private TextArea textStory;
+}
